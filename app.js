7ba90539a1a129753ff81d221b907b0fc86b0e07
Brian Axel <81195041+brianaxels2@users.noreply.github.com>
2022-01-13 02:07:23 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const pokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const myFunction = () => {
+
+  const pokemonPromisses = []
+
+  for(let i = 1; i <= 150; i++){
+    pokemonPromisses.push(fetch(pokemonUrl(i)).then(Response => Response.json()))
+  }
+
+  Promise.all(pokemonPromisses)
+    .then(pokemons => {
+      const liPokemons = pokemons.reduce((accumulator, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+        accumulator += `
+          <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png" />
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')}</p>
+          </li>
+        `
+        return accumulator
+      }, '')
+
+      const ul = document.querySelector('[data-js="pokedex"]')
+
+      ul.innerHTML = liPokemons
+
+      console.log(pokemonPromisses)
+
+    })
+
+}
+myFunction()
